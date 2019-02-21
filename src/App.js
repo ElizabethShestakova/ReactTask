1fd9eebd49e5143f244ea46bf526cbe46bfafed4
Liza <shestakova.e.y@gmail.com>
2019-02-21 19:50:00 +0300

    Загрузка данных

--- src/App.js
+++ src/App.js
@@ -1,29 +1,84 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loader from './Loader/Loader';
+import Table from './Table/Table';
 
 class App extends Component {
+
+  state = {
+    isLoading: true,
+    data: [],
+  };
+
+  async componentDidMount() {
+    const response = await fetch ('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}');    
+    const data = await response.json();
+    //  console.log(data)
+    this.setState({
+      isLoading: false,
+      data //совпадают ключ и значение
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-      
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+<div className="container">
+      {
+        this.state.isLoading ? <Loader /> : <Table data={this.state.data}/>
+      }
       </div>
     );
   }
+
 }
 
 export default App;
+
+
+// class App extends Component {
+  
+//   downloadSmallData() {
+//     fetch('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+//     .then( (response) => {        
+//          let data =  response.json(); 
+          
+
+         
+//     });
+//   }
+
+//   downloadBigData() {
+    
+//   }
+
+//   makeTable() {
+
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         <div className="buttons">        
+//           <button onClick={this.downloadSmallData} className="btn smallData">Маленький объем данных</button>
+//           <button onClick={this.downloadBigData} className="btn bigData">Большой объем данных</button>
+//         </div>
+//         <div className="table">
+//           <table>
+//             <tr>
+//               <th>id</th>
+//               <th>firstName</th>
+//               <th>lastName</th>
+//               <th>email</th>
+//               <th>phone</th>
+//             </tr>
+            
+//           </table>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
